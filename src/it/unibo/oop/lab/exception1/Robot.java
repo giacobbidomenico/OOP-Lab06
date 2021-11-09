f4703fce24c6f60837a425860d7c469cb9a60419
Domenico Giacobbi <domenico.giacobbi@studio.unibo.it>
2021-11-09 19:35:41 +0100

    Update of the class Robot to work with NotEnoughBatteryException

--- src/it/unibo/oop/lab/exception1/Robot.java
+++ src/it/unibo/oop/lab/exception1/Robot.java
@@ -75,17 +75,14 @@ public class Robot {
      *            the new Y position to move the robot to
      * @return true if robot gets moved, false otherwise
      */
-    private boolean moveToPosition(final int newX, final int newY) {
-        boolean returnValue = true;
+    private void moveToPosition(final int newX, final int newY) {
         if (this.isBatteryEnoughToMove()) {
             	this.environment.move(newX, newY);
                 this.consumeBatteryForMovement();
                 this.log("Moved to position(" + newX + "," + newY + ").");
         }else {
-            this.log("Can not move to position(" + newX + "," + newY + "). Not enough battery.");
-            returnValue = false;
+            throw new NotEnoughBatteryException(this.batteryLevel, MOVEMENT_DELTA_CONSUMPTION);
         }
-        return returnValue;
     }
 
     /**
